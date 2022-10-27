0ad5725e54b24c7856733bef8e68890efef794fd
KhangTran28 <94086792+KhangTran28@users.noreply.github.com>
2022-10-27 21:24:40 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop huy";
+$name ="Điện thoai";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span> 69/68 Đặng Thùy Trâm,P13,Bình Thạnh</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 0523777623</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> onepetforpet@gmail.com</p>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.792377018625!2d106.70114311541576!3d10.827194961213685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175288b870d57d1%3A0x4c6e58e94874db89!2zNjksIDY4IMSQLiDEkOG6t25nIFRodeG7syBUcsOibSwgUGjGsOG7nW5nIDEzLCBCw6xuaCBUaOG6oW5oLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmggNzAwMDAsIFZp4buHdCBOYW0!5e0!3m2!1svi!2sus!4v1666811620503!5m2!1svi!2sus" width="1100" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer>
+		<div class="container">
+			<div class="wrap-footer">
+				<div class="row">
+					<div class="col-md-3 col-footer footer-1">
+						<img src="images/logo2.png"></img>
+					</div>
+					</div>
+					<h3><span class="glyphicon glyphicon-home"></span> 69/68 Đặng Thùy Trâm,P13,Q.Bình Thạnh, Tp.HCM, Việt Nam.</h3>
+					<h3>SDT: 0523777623</h3>
+					<h3>Gmail: onepetforpets@gmail.com</h3>
+					<img src="images/bct2.png"></img>
+			</div>
+		</div>
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						 <h4>Copyright © 2022<h4>
+
+					</div>
+					<div class="col-md-6">
+						
+							<h4>Thiên đường giành cho thú cưng</h4>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
